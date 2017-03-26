b707eb0b4d7275876c74fd12397a428f5aa5353b
Bérénice Batut <bebatut@gmail.com>
2017-03-26 21:09:58 +0200

    Fix flake8

--- setup.py
+++ setup.py
@@ -1,24 +1,20 @@
-import os
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 
 setup(
-    name = "ebisearch",
-    version = "0.0.1",
-    author = "Bérénice Batut",
-    author_email = "berenice.batut@gmail.com",
-    description = ("A Python library for interacting with EBI Search's API"),
-    license = "MIT",
-    keywords = "api api-client ebi",
-    url = "https://github.com/bebatut/ebisearch",
+    name="ebisearch",
+    version="0.0.1",
+    author="Bérénice Batut",
+    author_email="berenice.batut@gmail.com",
+    description=("A Python library for interacting with EBI Search's API"),
+    license="MIT",
+    keywords="api api-client ebi",
+    url="https://github.com/bebatut/ebisearch",
     packages=find_packages(),
     entry_points={
           'console_scripts': [
               'ebisearch = ebisearch.__main__:main'
           ]
       },
-    install_requires=[
-        'Click',
-    ],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Environment :: Web Environment",
